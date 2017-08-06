59722024d62b028f08afd77f7717161c81550b4e
Your Name <you@example.com>
2017-08-06 14:55:58 +0300

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+устанавливаем express глобально: npm i express -g
+для создание базовой структуры устанавливаем express-generator: npm i express-generator -g
+для папки проэкта указываем: express --view=pug --css=sass
+устанавливаем все пакеты: npm i
+устанавливаем логер: npm i nodemon -g
---
+++ app.js
@@ -0,0 +1,27 @@
+let app = require('./settings');
+
+let index = require('./routes/index');
+let users = require('./routes/users');
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use( (req, res, next) => {
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+module.exports = app;
